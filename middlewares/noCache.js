518474616e828f5c8c7be3917a564f9eba96ebe7
Basithmgm <basithkmgm@gmail.com>
2026-02-06 14:33:34 +0530

    second week product listing

--- middlewares/noCache.js
+++ middlewares/noCache.js
@@ -1,7 +1,7 @@
-// Middleware to prevent caching of sensitive pages (login/signup)
 module.exports = (req, res, next) => {
-    res.set('Cache-Control', 'no-store, no-cache, must-revalidate, private');
+    res.set('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
     res.set('Pragma', 'no-cache');
     res.set('Expires', '0');
+    res.set('Surrogate-Control', 'no-store');
     next();
 };
